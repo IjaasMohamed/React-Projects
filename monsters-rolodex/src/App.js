85fe1481424ef59472acc745997c85c04d7aa735
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-20 02:37:11 +0530

    search

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -25,6 +26,10 @@ class App extends Component {
       );
   }
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
@@ -32,28 +37,14 @@ class App extends Component {
           type="search"
           placeholder="Search Monster"
           onChange={(event) => {
-            console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
             });
-            this.setState( () => {
-              return {monsters: filteredMonsters}
-            })
-
-            // const searchString = event.target.value.toLocaleLowerCase();
-
-            // const filteredMonsters = this.state.monsters.filter((monster) => {
-            //   return monster.name.toLocaleLowerCase().includes(searchString);
-            // });
-
-            // this.setState  (() => {
-            //   return {monsters:filteredMonsters};
-            // } )
           }}
         />
 
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               {" "}
