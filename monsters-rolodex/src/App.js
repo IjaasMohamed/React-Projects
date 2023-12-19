ddf3755cae515af88919edc262f95704eb5583bb
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-19 20:31:28 +0530

    fetching

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,4 @@
- import { Component } from "react";
+import { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -7,33 +7,35 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name:'Jika',
-          id:'125saasdssads'
-        },
-        {
-          name:'Muka',
-          id:'12sadsads'
-        },
-        {
-          name:'Meca',
-          id:'125sads'
-        },
-        {
-          name:'Kika',
-          id:'125sad'
-        },
-      ]
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-      {this.state.monsters.map((monst) => {
-        return <div key={monst.id}><h1>{monst.name}</h1></div>
-      })}  
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
       </div>
     );
   }
