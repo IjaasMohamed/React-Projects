4949f3c87c0886615e6d60cb992bc27435d8b5eb
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-12-21 20:57:57 +0530

    working with components

---
+++ monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class CardList extends Component {
+
+    render(){
+
+        console.log(this)
+        return (
+            <div>Hello from cardList component</div>
+        )
+    }
+}
+
+export default CardList;
